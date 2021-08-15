21605767d97dd16af09e5e0c61fa98e2c6a7b539
krishna <krishnamsmail@gmail.com>
2021-08-15 16:54:01 -0400

    Using Recursion

---
+++ src/main/java/com/learnjava/forkjoin/ForkJoinUsingRecursion.java
@@ -0,0 +1,46 @@
+package com.learnjava.forkjoin;
+
+import com.learnjava.util.DataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ForkJoinUsingRecursion extends RecursiveTask<List<String>> {
+
+    private List<String> inputList;
+
+    public ForkJoinUsingRecursion(List<String> inputList) {
+        this.inputList = inputList;
+    }
+
+    public static void main(String[] args) {
+
+
+        stopWatch.start();
+        List<String> resultList = new ArrayList<>();
+        List<String> names = DataSet.namesList();
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        ForkJoinUsingRecursion forkJoinUsingRecursion = new ForkJoinUsingRecursion(names);
+        forkJoinPool.invoke(forkJoinUsingRecursion);
+        stopWatch.stop();
+        log("Final Result : "+ resultList);
+        log("Total Time Taken : "+ stopWatch.getTime());
+    }
+
+
+    private static String addNameLengthTransform(String name) {
+        delay(500);
+        return name.length()+" - "+name ;
+    }
+
+    @Override
+    protected List<String> compute() {
+        return null;
+    }
+}
